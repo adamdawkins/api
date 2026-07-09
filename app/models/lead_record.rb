92183703936f489a0750a7cfd8e018cd149bb41c
Adam Dawkins <adam@dragondrop.uk>
2026-07-09 23:39:04 +0100

    f Add LeadRecord, OfficeRecord has_many relations

--- app/models/lead_record.rb
+++ app/models/lead_record.rb
@@ -4,4 +4,8 @@ class LeadRecord < ApplicationRecord
   self.table_name = "leads"
 
   belongs_to :office, class_name: "OfficeRecord", optional: true
+
+  has_many :projects, class_name: "ProjectRecord",
+           foreign_key: :lead_id,
+           dependent: :destroy
 end
--- app/models/office_record.rb
+++ app/models/office_record.rb
@@ -2,4 +2,13 @@
 
 class OfficeRecord < ApplicationRecord
   self.table_name = "offices"
+
+  has_many :leads,
+           class_name: "LeadRecord",
+           foreign_key: :office_id,
+           dependent: :destroy
+  has_many :projects,
+           class_name: "ProjectRecord",
+           foreign_key: :office_id,
+           dependent: :destroy
 end
