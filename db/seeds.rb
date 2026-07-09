24b7fbcff2305cc8b0927d05291a6f3bf90ee5d0
Adam Dawkins <adam@dragondrop.uk>
2026-07-09 23:39:37 +0100

    e Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,8 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-office = OfficeRecord.find_or_create_by!(key: "LDN", name: "London")
-lead = LeadRecord.find_or_create_by!(api_id: "lead_e2e_1") { |l| l.office = office }
-ProjectRecord.find_or_create_by!(api_id: "prj_e2e_1") do |project|
-  project.lead = lead
-  project.office = office
-end
+OfficeRecord.destroy_all
+
+office = OfficeRecord.find_or_create_by!(key: "NJC", name: "New Jersey Central")
+lead = LeadRecord.find_or_create_by!(api_id: "lead_abc", office:)
+ProjectRecord.find_or_create_by!(api_id: "prj_abc", lead:, office:)
