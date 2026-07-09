bad3ad338489525c4d942228ecb5c015b0fea924
Adam Dawkins <adam@dragondrop.uk>
2026-07-09 21:45:42 +0100

    Return project at GET /api/v1/projects/:api_id

--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -1,15 +1,25 @@
 require "rails_helper"
 
 RSpec.describe 'Projects API', type: :request do
-  describe "GET /api/v1/projects/1.json" do
-    subject(:req) { get "/api/v1/projects/1.json" }
-    context "with a project at the id" do
-    it "returns a 200" do
-      req
-      expect(response).to have_http_status(200)
-    end
+  describe "GET /api/v1/projects/:api_id" do
+    subject(:req) { get "/api/v1/projects/prj_123" }
+    context "with a matching project" do
+      before { create(:project_record, api_id: "prj_123") }
 
-    it "returns the project"
+      it "returns a 200" do
+        req
+
+        expect(response).to have_http_status(200)
+      end
+
+      it "returns the project" do
+        req
+
+        json = JSON.parse(response.body)
+        puts json["project"]
+
+        expect(json["project"]["api_id"]).to eq("prj_123")
+      end
     end
 
     context "without a project at the id" do
