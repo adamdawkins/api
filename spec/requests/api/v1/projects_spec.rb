b219bf9f5bcf04cc1a08bce428d08a41ebcf6836
Adam Dawkins <adam@dragondrop.uk>
2026-07-10 22:30:48 +0100

    t update request spec to check for address body

--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe 'Projects API', type: :request do
     subject(:req) { get "/api/v1/projects/prj_123" }
     context "with a matching project" do
       before do
-        lead = create(:lead_record, first_name: "John", last_name: "Doe")
+        town = create(:town_record, name: "Philadelphia")
+        zipcode_record = create(:zipcode_record,
+                                code: "12345",
+                                town:,
+                                state: "PA")
+        lead = create(:lead_record,
+                      street_address: "123 Main St",
+                      first_name: "John",
+                      last_name: "Doe",
+                      zipcode_record:)
         create(:project_record, api_id: "prj_123", lead:)
       end
 
@@ -38,6 +47,17 @@ RSpec.describe 'Projects API', type: :request do
 
         expect(json["project"]["customer"]["first_name"]).to eq("John")
         expect(json["project"]["customer"]["last_name"]).to eq("Doe")
+      end
+
+      it "returns the customer's address" do
+        req
+
+        customer_json = JSON.parse(response.body)["project"]["customer"]
+
+        expect(customer_json["address"]["line1"]).to eq("123 Main St")
+        expect(customer_json["address"]["city"]).to eq("Philadelphia")
+        expect(customer_json["address"]["state"]).to eq("PA")
+        expect(customer_json["address"]["zipcode"]).to eq("12345")
       end
     end
 
