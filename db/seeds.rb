6092210ac87aa53fb8f4050b9d617dd81e9900b7
Adam Dawkins <adam@dragondrop.uk>
2026-07-10 22:42:35 +0100

    e Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,20 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-TownRecord.destroy_all
+LeadRecord.destroy_all
 ZipcodeRecord.destroy_all
+TownRecord.destroy_all
 OfficeRecord.destroy_all
 
 town = TownRecord.create!(name: "Newark")
-zipcode = ZipcodeRecord.create!(code: "07101", town:)
+zipcode = ZipcodeRecord.create!(code: "07101", town:, state: "NJ")
 
 office = OfficeRecord.find_or_create_by!(key: "NJC", name: "New Jersey Central")
 lead = LeadRecord.find_or_create_by!(api_id: "lead_abc",
                                      office:,
                                      first_name: "John",
                                      last_name: "Doe",
-                                     zipcode: "07101")
+                                     zipcode: "07101",
+                                     street_address: "123 Main St",
+                                    )
 ProjectRecord.find_or_create_by!(api_id: "prj_abc", lead:, office:)
