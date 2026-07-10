0c866549b44eb91f1a19a7fdfdcd565238cdd6d5
Adam Dawkins <adam@dragondrop.uk>
2026-07-10 17:10:03 +0100

    f Add lead dependency on zipcode

--- app/models/lead_record.rb
+++ app/models/lead_record.rb
@@ -4,6 +4,10 @@ class LeadRecord < ApplicationRecord
   self.table_name = "leads"
 
   belongs_to :office, class_name: "OfficeRecord", optional: true
+  belongs_to :zipcode_record,
+             primary_key: "code",
+             foreign_key: "zipcode",
+             class_name: "ZipcodeRecord"
 
   has_many :projects, class_name: "ProjectRecord",
            foreign_key: :lead_id,
