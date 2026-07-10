72b2e9864a63584e2585def09e3cab171b500a2c
Adam Dawkins <adam@dragondrop.uk>
2026-07-10 15:18:37 +0100

    f Include Project#customer from ProjectRepo.by_api_id

--- app/repos/project_repo.rb
+++ app/repos/project_repo.rb
@@ -1,10 +1,13 @@
 class ProjectRepo
   def self.by_api_id(api_id)
-    record = ProjectRecord.includes(:office).find_by!(api_id:)
+    record = ProjectRecord.includes(:office, :lead).find_by!(api_id:)
+
     Project.new(api_id: record.api_id,
                 office_key: record.office.key,
                 id: record.id,
-                status: status_from_db(record.status))
+                status: status_from_db(record.status),
+                customer: Customer.new(first_name: record.lead.first_name, last_name: record.lead.last_name)
+               )
   end
 
   # DB stores legacy title-cased strings with uppercased acronyms,
