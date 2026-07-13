3bd99862ca34fc207bfb6f8247bbd2804f831167
Adam Dawkins <adam@dragondrop.uk>
2026-07-13 23:13:46 +0100

    f Add types to Orange::Flows::Finance::Decline

--- lib/orange/flows/finance/decline.rb
+++ lib/orange/flows/finance/decline.rb
@@ -1,7 +1,12 @@
+# typed: true
+
 module Orange
   module Flows
     module Finance
       class Decline
+        extend T::Sig
+
+        sig { params(project: FinanceProject).returns(Results::Result) }
         def call(project)
           unless project.status == Project::Status::FinanceApproval
             return Results::Failure.new(:wrong_status)
