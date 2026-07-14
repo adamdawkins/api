3e8d1b025eef8f7f1e581555e9c948bc7d51dce6
Adam Dawkins <adam@dragondrop.uk>
2026-07-14 21:08:47 +0100

    f Add Projects::FinanceController#approve

--- app/controllers/api/v1/projects/finance_controller.rb
+++ app/controllers/api/v1/projects/finance_controller.rb
@@ -2,6 +2,17 @@ module Api
   module V1
     module Projects
       class FinanceController < ApplicationController
+        def approve
+          result = ::Projects::Finance::Actions::Approve.new.call(project_api_id: params[:api_id])
+
+          case result
+          when Results::Success
+            render json: { project: result.value!.as_json }, status: :ok
+          when Results::Failure
+            render json: { error: result.failure }, status: :unprocessable_entity
+          end
+        end
+
         def decline
           result = ::Projects::Finance::Actions::Decline.new.call(project_api_id: params[:api_id])
 
