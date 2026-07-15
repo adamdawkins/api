5100b7f9876b69781c43c45d66dd8209a6f5a3dc
Adam Dawkins <adam@dragondrop.uk>
2026-07-15 17:39:39 +0100

    Add action to controller

--- app/controllers/api/v1/projects/accounting_controller.rb
+++ app/controllers/api/v1/projects/accounting_controller.rb
@@ -2,7 +2,18 @@ module Api
   module V1
     module Projects
       class AccountingController < ApplicationController
-        def complete; end
+        def complete
+          result = ::Projects::Accounting::Actions::Complete.new.call(
+            project_api_id: params[:api_id]
+          )
+
+          case result
+          when Results::Success
+            render json: { project: result.value!.as_json }, status: :ok
+          when Results::Failure
+            render json: { error: result.failure }, status: :unprocessable_entity
+          end
+        end
       end
     end
   end
