898846280dec7f1bb7c628c3e0bbd390de5e99b7
Adam Dawkins <adam@dragondrop.uk>
2026-07-16 17:44:28 +0100

    r Remove unused ProjectRepo.by_api_id

--- app/repos/project_repo.rb
+++ app/repos/project_repo.rb
@@ -25,25 +25,6 @@ class ProjectRepo
                  )
     end
 
-    sig { params(api_id: String).returns(Orange::Project) }
-    def by_api_id(api_id)
-      record = ProjectRecord
-        .joins(:office, lead: { zipcode_record: :town })
-        .select(:id, :api_id, :status,
-                offices: { key: :office_key },
-                leads: [ :first_name, :last_name, :street_address ],
-                town: { name: :city },
-                zipcodes: { code: :zipcode, state: :state }
-               ).find_by!(api_id:)
-
-      Orange::Project.new(api_id: record.api_id,
-                  office_key: record.office_key,
-                  id: record.id,
-                  status: status_from_db(record.status),
-                  customer: customer(record)
-                 )
-    end
-
     private
 
     # This can't be typed to `ProjectRecord` because of how ActiveRecord
