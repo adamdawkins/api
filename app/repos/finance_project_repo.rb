c9b4e2e2e33983a599e8d5d09a9813383c245597
Adam Dawkins <adam@dragondrop.uk>
2026-07-16 17:48:44 +0100

    r Remove FinanceProjectRepo.by_api_id

--- app/repos/finance_project_repo.rb
+++ app/repos/finance_project_repo.rb
@@ -13,14 +13,6 @@ class FinanceProjectRepo
                                  status: status_from_db(record.status))
     end
 
-    sig { params(api_id: String).returns(Orange::FinanceProject) }
-    def by_api_id(api_id)
-      record = ProjectRecord.find_by!(api_id:)
-
-      Orange::FinanceProject.new(id: record.id,
-                                 status: status_from_db(record.status))
-    end
-
     sig do
       params(project_id: Integer, status: Orange::Project::Status).returns(Orange::FinanceProject)
     end
--- spec/repos/finance_project_repo_spec.rb
+++ spec/repos/finance_project_repo_spec.rb
@@ -25,30 +25,6 @@ RSpec.describe FinanceProjectRepo do
       end
     end
   end
-  describe ".by_api_id" do
-    before { create(:project_record, id: 1, api_id: "prj_123", status:) }
-
-    let(:status) { "Finance Approval" }
-
-    let(:expected_project) do
-      Orange::FinanceProject.new(id: 1, status: Orange::Project::Status::FinanceApproval)
-    end
-
-    context "with a project with the api id" do
-      it "returns an Orange::FinanceProject matching the api id" do
-        project = described_class.by_api_id("prj_123")
-        expect(project).to eq (expected_project)
-      end
-    end
-
-    context "without a project with the api id" do
-      it "raises ActiveRecord::RecordNotFound" do
-        expect do
-          described_class.by_api_id("prj_999")
-        end.to raise_error(ActiveRecord::RecordNotFound)
-      end
-    end
-  end
 
   describe ".update_status" do
     let(:project_record) { create(:project_record, id: 1, status: "Finance Approval") }
